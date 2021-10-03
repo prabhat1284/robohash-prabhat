75e88cdaf9cb79c8f64ea16884a8fdcc4e580f90
Prabhat <prabhat1284@gmail.com>
2021-10-03 13:58:18 +0530

    roboHash written using Promises

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const superagent = require('superagent')
 const fs = require('fs')
 
+2.
+function roboHashPromise(){
+    data = Math.random().toString(36).substring(2,7); 
+    return new Promise((resolve,reject) => {
+         resolve(data)       
+    })
+}
+
+function writeFilePromise(filelocation,data) {
+    return new Promise((resolve,reject) => {
+        fs.writeFile(filelocation, data, (err) => {
+            if (err) {
+                reject("not able to write inside the file")
+            }
+            resolve()
+        })
+    })
+}
+//Promise Chaining
+roboHashPromise().then((data)=>{
+    console.log(`random string ${data}`)
+    return superagent.get(`https://robohash.org/${data}`)
+})
+.then((res)=>{
+    console.log(res.request.url)
+    return writeFilePromise("./robotImage.txt",res.request.url)
+})
+.then(()=>{
+    console.log('successfully written')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
 //1.Using Callback
-const roboHash=(err,data)=>{
-    data = Math.random().toString(36).substring(2,7);
-    superagent
-    .get(`https://robohash.org/${data}`)
-    .end((err,res)=>{
-        console.log(res.request.url);
-        fs.writeFile("./robotImage.txt",res.request.url,(err)=>{
-            console.log("Image url sucessfully written inside the file");
-        });
-    });
-};
-roboHash()
+// const roboHash=(err,data=>{})=>{
+//     data = Math.random().toString(36).substring(2,7);
+//     superagent
+//     .get(`https://robohash.org/${data}`)
+//     .end((err,res)=>{
+//         console.log(res.request.url);
+//         fs.writeFile("./robotImage.txt",res.request.url,(err)=>{
+//             console.log("Image url sucessfully written inside the file");
+//         });
+//     });
+// };
+// roboHash()
